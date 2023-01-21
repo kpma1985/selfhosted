46d9e9e5bc0e6ee5893f80249375018734d6719b
avirut <avirut.mehta@gmail.com>
2023-01-21 17:26:26 -0600

    hostmap ignored

--- apps/caddy-ingress/reload_caddy_json.sh
+++ apps/caddy-ingress/reload_caddy_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 hostmap_to_caddy.py > ./caddy-ingress.json
+python3 convert_hostmap_to_caddy.py > ./caddy-ingress.json
 docker exec caddy-ingress caddy reload --config /etc/caddy/caddy-ingress.json
